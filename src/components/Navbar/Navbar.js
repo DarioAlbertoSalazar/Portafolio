7117787d72c5b5db3533855329ca54be24a51e1b
Darío Salazar Leal <dario12_58@hotmail.com>
2023-05-26 19:43:06 -0600

    feat:navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./Navbar.styles";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+  const clickItemNav = (item) => {
+    console.log(item);
+    document.getElementById(item).scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <S.ContainerNavbar>
+      <S.ContainerLogo>
+        <div>
+          <h1>Darío Salazar</h1>
+          <h2>Frontend Developer</h2>
+        </div>
+      </S.ContainerLogo>
+
+      <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+      </S.ContainerBurguer>
+
+      <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+        <p onClick={() => clickItemNav("about-me")}>About me</p>
+        <p onClick={() => clickItemNav("projects")}>Projects</p>
+        <S.Button onClick={() => clickItemNav("contact")}>
+          🤙 Contact me
+        </S.Button>
+      </S.ContainerItems>
+    </S.ContainerNavbar>
+  );
 };
 
 export default Navbar;
